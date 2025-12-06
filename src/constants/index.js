c5c528ad3d551ad47777c0f20fdde5edc5aef9df
HarshKumar7687 <drharshkumar499@gmail.com>
2025-12-06 12:07:36 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,325 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    href: "#home",
+  },
+  {
+    id: 2,
+    name: "About",
+    href: "#about",
+  },
+  {
+    id: 3,
+    name: "Work",
+    href: "#work",
+  },
+  {
+    id: 4,
+    name: "Contact",
+    href: "#contact",
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: "Emily Johnson",
+    position: "Marketing Director at GreenLeaf",
+    img: "assets/review1.png",
+    review:
+      "Working with Harsh was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+  },
+  {
+    id: 2,
+    name: "Mark Rogers",
+    position: "Founder of TechGear Shop",
+    img: "assets/review2.png",
+    review:
+      "Harsh’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.",
+  },
+  {
+    id: 3,
+    name: "John Dohsas",
+    position: "Project Manager at UrbanTech ",
+    img: "assets/review3.png",
+    review:
+      "I can’t say enough good things about Harsh. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
+  },
+  {
+    id: 4,
+    name: "Ether Smith",
+    position: "CEO of BrightStar Enterprises",
+    img: "assets/review4.png",
+    review:
+      "Harsh was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.",
+  },
+];
+
+export const myProjects = [
+  {
+    title: "TrashFormers - Smart Waste Management System",
+    desc: "TrashFormers is an AI-powered waste identification and management platform. It allows users to upload images of waste, calculates eco-coins as rewards, and updates real-time waste status while ensuring transparency in recycling operations.",
+    subdesc:
+      "Built as a ReactJS, Tailwind CSS, NodeJS, ExpressJS and MongoDB, TrashFormers is designed for optimal performance and scalability.",
+    href: "https://trash-former.netlify.app/",
+    texture: "https://files.catbox.moe/carveu.mp4",
+    logo: "/assets/logo1.png",
+    logoStyle: {
+      backgroundColor: "#2A1816",
+      border: "0.2px solid #36201D",
+      boxShadow: "0px 0px 60px 0px #AA3C304D",
+    },
+    spotlight: "/assets/spotlight1.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "NodeJS",
+        path: "/assets/nodejs.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+      {
+        id: 5,
+        name: "JavaScript",
+        path: "/assets/js.png",
+      },
+    ],
+  },
+  {
+    title: "Connexa - A social media platform for professionals",
+    desc: "Connexa is a powerful collaborative app that elevates the capabilities of real-time chating. As an enhanced version of Instagram, It supports millions of posts simultaneously, ensuring that every change is captured instantly and accurately.",
+    subdesc:
+      "With Connexa, users can experience the future of social media, where multiple users can work together in real time without any lag, by using ReactJS, NodeJS,ExpressJS and MongoDB newest features.",
+    href: "https://connexa-0mua.onrender.com",
+    texture: "https://files.catbox.moe/op868i.mp4",
+    logo: "/assets/logo2.png",
+    logoStyle: {
+      backgroundColor: "#13202F",
+      border: "0.2px solid #17293E",
+      boxShadow: "0px 0px 60px 0px #2F6DB54D",
+    },
+    spotlight: "/assets/spotlight2.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "JavaScript",
+        path: "/assets/js.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+      {
+        id: 5,
+        name: "NodeJS",
+        path: "/assets/nodejs.png",
+      },
+    ],
+  },
+  {
+    title: "Jeans - E Commerce Platform",
+    desc: "Jeans is a modern e-commerce platform focused exclusively on premium jeans shopping. It provides a seamless and intuitive shopping experience with advanced product filtering, size guides, wishlist options, and secure checkout.",
+    subdesc:
+      "Built with responsive front-end design and integrated cart functionality, DenimWave delivers a smooth browsing and purchasing experience across all devices.",
+    href: "https://jeans-frontend.onrender.com/",
+    texture: "https://files.catbox.moe/p7xmit.mp4",
+    logo: "/assets/logo3.png",
+    logoStyle: {
+      backgroundColor: "#60f5a1",
+      background:
+        "linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)",
+      border: "0.2px solid rgba(208, 213, 221, 1)",
+      boxShadow: "0px 0px 60px 0px rgba(35, 131, 96, 0.3)",
+    },
+    spotlight: "/assets/spotlight3.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/js.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+      {
+        id: 5,
+        name: "NodeJS",
+        path: "/assets/nodejs.png",
+      },
+    ],
+  },
+  {
+    title: "Cygnus - Online Video Conferencing Platform",
+    desc: "Cygnus is a high-performance video conferencing platform designed for seamless virtual communication. It supports HD calling, real-time messaging, screen sharing, participant controls, and secure room authentication.",
+    subdesc:
+      "Built using WebRTC, NextJS, and scalable backend logic, Cygnus delivers a smooth collaborative experience with low latency and strong privacy standards — ideal for meetings, online classes, and remote teams.",
+    href: "https://cygnus-5b8t.vercel.app/",
+    texture: "https://files.catbox.moe/m05bin.mp4",
+    logo: "/assets/logo4.png",
+    logoStyle: {
+      backgroundColor: "#0E1F38",
+      border: "0.2px solid #0E2D58",
+      boxShadow: "0px 0px 60px 0px #2F67B64D",
+    },
+    spotlight: "/assets/spotlight4.png",
+    tags: [
+      {
+        id: 1,
+        name: "Next.js",
+        path: "/assets/next.png",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "ZegoCloud",
+        path: "/assets/zego.png",
+      },
+    ],
+  },
+  {
+    title: "ChessMaster - Strategic Multiplayer Chess Game",
+    desc: "ChessMaster is an interactive web-based chess platform that allows players to practice, compete, and refine their strategic gameplay. It features legal move validation, checkmate detection, player turns, and a clean visual interface.",
+    subdesc:
+      "Built using HTML, Tailwind CSS, JavaScript,NodeJS and ExpressJS ChessMaster offers smooth gameplay mechanics with responsive UI, making it suitable for both beginners and advanced chess enthusiasts.",
+    href: "https://chess-hqsi.onrender.com",
+    texture: "https://files.catbox.moe/a0tzns.mp4",
+    logo: "/assets/logo5.png",
+    logoStyle: {
+      backgroundColor: "#1C1A43",
+      border: "0.2px solid #252262",
+      boxShadow: "0px 0px 60px 0px #635BFF4D",
+    },
+    spotlight: "/assets/spotlight5.png",
+    tags: [
+      {
+        id: 1,
+        name: "nodeJs",
+        path: "/assets/nodejs.png",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "JavaScript",
+        path: "/assets/js.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall
+      ? [4, -5, 0]
+      : isMobile
+      ? [5, -5, 0]
+      : isTablet
+      ? [5, -5, 0]
+      : [9, -5.5, 0],
+    reactLogoPosition: isSmall
+      ? [3, 4, 0]
+      : isMobile
+      ? [5, 4, 0]
+      : isTablet
+      ? [5, 4, 0]
+      : [12, 3, 0],
+    ringPosition: isSmall
+      ? [-5, 7, 0]
+      : isMobile
+      ? [-10, 10, 0]
+      : isTablet
+      ? [-12, 10, 0]
+      : [-24, 10, 0],
+    targetPosition: isSmall
+      ? [-5, -10, -10]
+      : isMobile
+      ? [-9, -10, -10]
+      : isTablet
+      ? [-11, -7, -10]
+      : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: "Tata Motors",
+    pos: "Lead Web Developer",
+    duration: "June 2025 - July 2025",
+    title:
+      "Completed an offline internship at Tata Motors where I led a team working on a full-stack MERN project. My role involved backend architecture, task management, code reviews, and ensuring smooth development workflows while meeting project deadlines.",
+    icon: "/assets/tata.svg",
+    animation: "salute",
+  },
+  {
+    id: 2,
+    name: "Code Clause",
+    pos: "Web Developer",
+    duration: "Oct 2024 - Nov 2024",
+    title:
+      "Worked as a Web Development intern at CodeClause where I developed and improved real-world web applications. Focused on creating responsive UI components, managing database operations, and integrating backend services while following clean code and agile practices.",
+    icon: "/assets/code.svg",
+    animation: "clapping",
+  },
+  {
+    id: 3,
+    name: "Freelance",
+    pos: "Web Developer",
+    duration: "2023 - Present",
+    title:
+      "Worked independently with clients and personal projects, focusing on building responsive and modern full-stack web applications. Gained experience in requirement gathering, UI/UX implementation, debugging, performance optimization, and deployment workflows.",
+    icon: "/assets/freelance.svg",
+    animation: "victory",
+  },
+];
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,47 @@
+const Footer = () => {
+  return (
+    <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
+      <div className="text-white-500 flex gap-2">
+        <p>Terms & Conditions</p>
+        <p>|</p>
+        <p>Privacy Policy</p>
+      </div>
+
+      <div className="flex gap-3">
+        <a href="https://github.com/HarshKumar7687">
+          <div className="social-icon">
+            <img
+              src="/assets/github.svg"
+              alt="github"
+              className="w-1/2 h-1/2"
+            />
+          </div>
+        </a>
+        <a href="https://www.linkedin.com/in/harsh-kumar-91aa8132b/">
+          <div className="social-icon">
+            <img
+              src="/assets/linkdein.svg"
+              alt="twitter"
+              className="w-1/2 h-1/2"
+            />
+          </div>
+        </a>
+        <a href="https://www.instagram.com/harshkumar_7687/">
+          <div className="social-icon">
+            <img
+              src="/assets/instagram.svg"
+              alt="instagram"
+              className="w-1/2 h-1/2"
+            />
+          </div>
+        </a>
+      </div>
+
+      <p className="text-white-500">
+        © 2025 Harsh Kumar. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
